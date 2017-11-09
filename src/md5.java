53dc5e5edbb7f2451f9daf0b74f9ad1c75d760e8
paulbartus <paul.bartus@upr.edu>
2017-11-09 14:54:52 -0400

    Initial version.

---
+++ src/md5.java
@@ -0,0 +1,19 @@
+import java.security.MessageDigest;
+
+public class md5 {
+
+    public static String getMD5(String value) {
+        try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(value.getBytes());
+            return bytesToHex(md.digest());
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+    private static String bytesToHex(byte[] bytes) {
+        StringBuffer result = new StringBuffer();
+        for (byte b : bytes) result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        return result.toString();
+    }
+}
